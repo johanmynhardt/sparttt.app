8c28a3c2b9e85e2a493857d26f177dda4ce77c92
Johan Mynhardt <johanmynhardt@gmail.com>
2019-07-22 22:57:06 +0200

    Remove unused deps declarations.

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,9 +1,1 @@
-{:foreign-libs
- [{:file "resources/public/libs/instascan.min.js"
-   :provides ["instascan"]
-   :global-exports
-   {instascan Instascan}}
-
-  {:file "resources/public/libs/qrcode.min.js"
-   :provides ["qrcode"]
-   :global-exports [qrcode QRCode]}]}
+{}
