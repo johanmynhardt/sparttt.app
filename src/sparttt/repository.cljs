155e63fe42df2dda803812aaa49c7657d35ea336
Johan Mynhardt <johanmynhardt@gmail.com>
2019-08-02 19:16:39 +0200

    Add `localStorage` backing to repository ns.

--- src/sparttt/repository.cljs
+++ src/sparttt/repository.cljs
@@ -1,12 +1,30 @@
-(ns sparttt.repository)
+(ns sparttt.repository
+  (:require
+    [cljs.reader]))
 
-(defonce repo (atom {:scans []}))
+(defonce repo
+  (atom
+    {:scans []
+     :journal []}))
+
+(def ^:dynamic storage-key "tt-repo")
+
+(defn- write-to-local-storage [col-key val]
+  (aset js/localStorage storage-key
+    (swap! repo update col-key
+      (fn [col] (conj col val)))))
+
+(defn- read-from-local-storage [col-key]
+  (-> js/localStorage
+    (aget storage-key)
+    cljs.reader/read-string
+    (get col-key)))
 
 (defn save-scan [val]
-  (swap! repo update :scans (fn [scans] (conj scans val))))
+  (write-to-local-storage :scans val))
+
+(defn journal-append [val]
+  (write-to-local-storage :journal val))
 
 (defn list-scans []
-  (let [scans
-        (get @repo :scans)]
-    (js/console.info "returning scans: " (count scans))
-    scans))
+  (read-from-local-storage :scans))
