aa0444182d8ab9518fd418907ccf3632b0c3931c
Johan Mynhardt <johanmynhardt@gmail.com>
2019-08-04 11:10:01 +0200

    Use regex from v1 for capturing athlete details and sequence numbers.

--- src/sparttt/scenes/scan.cljs
+++ src/sparttt/scenes/scan.cljs
@@ -8,7 +8,8 @@
     [Instascan.Scanner]
     [rum.core :as rum]
     [sparttt.repository :as repository]
-    [sparttt.scenes.settings :as settings]))
+    [sparttt.scenes.settings :as settings]
+    [sparttt.browser-assist :as browser-assist]))
 
 (def athlete-details (atom nil))
 (def athlete-sequence (atom nil))
@@ -78,7 +79,7 @@
         (fn [content]
           (.stop scanner)
           (hide-video-modal)
-          (println "got content: " content)
+          (repository/journal-append {:inst (time/now) :on-scan content})
           (on-scan content))))
     (with-camera
       (fn [camera]
@@ -86,24 +87,39 @@
           (.start camera))
         (show-video-modal)))))
 
+(def athlete-regex #"^([\w\ \-'`]+){1}:(.+){1}$")
 (defn capture-athlete []
-  ;; TODO: use regex from V1 to verify athlete name/id.
   (capture-qr
     (fn [content]
-      (let [[_ nm id] (re-matches #"(.*):(.*)" content)]
-        ;; TODO: get correct regex for extracting groups
-        (swap! athlete-details assoc
-          :name nm
-          :id id
-          :tstamp (time/now))))))
+      (let [[_ athlete ident] (re-matches athlete-regex content)]
+        (cond
+          (and athlete ident)
+          (swap! athlete-details assoc
+            :name athlete
+            :id ident
+            :tstamp (time/now))
 
+          :else
+          (do
+            (browser-assist/speak "Sorry, I couldn't read a user from the input!")
+            (js/alert (str "`" content "` did not match `athlete-regex`."))))))))
+
+(def sequence-regex #"^(\d+)$")
 (defn capture-sequence []
-  ;; TODO: use regex from V1 to verify sequence
   (capture-qr
     (fn [content]
-      (swap! athlete-sequence assoc
-        :seq content
-        :tstamp (time/now)))))
+      (let [[_ sequence] (re-matches sequence-regex content)]
+        (cond
+          sequence
+          (swap! athlete-sequence assoc
+            :seq content
+            :tstamp (time/now))
+
+          :else
+          (do
+            (browser-assist/speak "Sorry, I couldn't read a sequence from the input!")
+            (js/alert
+              (str "`" content "` does not match `sequence-regex`."))))))))
 
 (defn discard-details []
   (reset! athlete-details nil)
@@ -125,46 +141,46 @@
 
 (rum/defc scene < rum/reactive []
   (reset! ascanner nil)
-  (let [det (rum/react athlete-details)
-        seq (rum/react athlete-sequence)
-        last (rum/react last-capture)
-        ]
+  (let [athlete (rum/react athlete-details)
+        sequence (rum/react athlete-sequence)
+        last-athlete (rum/react last-capture)]
+
     [:div
 
      [:div.video-modal {:hidden true}
       [:video#preview]
-      [:button {:on-click
-                #(let [scanner (get-scanner)]
-                   (println "Stopping scanner " scanner)
-                   (.stop scanner)
-                   (hide-video-modal))} "stop scan"]]
+      [:button
+       {:on-click
+        #(let [scanner (get-scanner)]
+           (println "Stopping scanner " scanner)
+           (.stop scanner)
+           (hide-video-modal))}
+       [:li.fas.fa-stop] " stop scan"]]
 
-     (when-not det
+     (when-not athlete
        [:div.card.with-gradient {:on-click capture-athlete}
         [:div [:li.fas.fa-address-card touch-icon-style]
          [:li.no-list "Capture Athlete"]]])
 
-     (when (and det (not seq))
+     (when (and athlete (not sequence))
        [:div.card.with-gradient {:on-click capture-sequence}
         [:div [:li.fas.fa-hashtag touch-icon-style]
          [:li.no-list "Capture Sequence"]]])
 
-
-
-     (when (and det seq)
+     (when (and athlete sequence)
        [:div.card
         [:div
          [:div [:b "Name:"]]
-         [:div [:p (:name det)]]
+         [:div [:p (:name athlete)]]
          #_[:br]
-         [:div [:b "Sequence:"] " " (:seq seq)]]
+         [:div [:b "Sequence:"] " " (:seq sequence)]]
         [:hr]
         [:button {:on-click persist-details}
-         [:li.fas.fa-save] " persist"] "|"
+         [:li.fas.fa-save] " persist"] " "
         [:button {:on-click discard-details}
          [:li.fas.fa-trash] " " "discard"]])
 
-     (when last
+     (when last-athlete
        [:div.card
         [:div.title [:li.fas.fa-check] " " "Last Stored"]
-        [:p (with-out-str (cljs.pprint/pprint last))]])]))
+        [:p (with-out-str (cljs.pprint/pprint last-athlete))]])]))
