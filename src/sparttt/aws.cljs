e8e3be428dfefd5656a2cf01edf34085d84e9153
Johan Mynhardt <johanmynhardt@gmail.com>
2020-05-02 21:01:02 +0200

    Use cljs-http.client for web calls.

--- src/sparttt/aws.cljs
+++ src/sparttt/aws.cljs
@@ -1,52 +1,68 @@
 (ns sparttt.aws
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require
    [clojure.string :as str]
-   [clojure.walk :as walk]))
+   [cljs.core.async :refer [<!]]
+   [cljs-http.client :as http]))
+
+(def default-config
+  {:api :js10g4adv4
+   :region :eu-west-1
+   :stage :beta
+   :default-event-id "no-event-id"})
 
 (def config
-  (atom
-   {:api :js10g4adv4
-    :region :eu-west-1
-    :stage :beta
-    :default-event-id "no-event-id"}))
+  (atom default-config))
+
+(defn config-from-edn [uri]
+  (go
+    (let [response (<! (http/get uri))]
+      (cond
+        (:success response)
+        (reset! config (merge @config (:body response)))
+        :else
+        (js/console.log
+         "Could not update config"
+         (clj->js
+          (select-keys response [:status :error-text :body])))))))
+
+(config-from-edn "/aws.edn")
 
 (defn build-uri [& path-parts]
   (let [{:keys [api region stage]} @config
-        base
-        (str "https://" (name api) ".execute-api." (name region) ".amazonaws.com/" (name stage))]
+        base (str "https://" (name api) ".execute-api." (name region) ".amazonaws.com/" (name stage))]
     (str/join "/" (cons base path-parts))))
 
 (defn fetch
   "Wrapper to js-ify cljs config and return a promise which parsed json response."
   [endpoint config & [on-resolve]]
-  (->
-   (js/fetch endpoint (clj->js config))
-   (.then #(.json %))
-   (.then #(walk/keywordize-keys (js->clj %)))
-   (.then
-    (fn [m]
+
+  (go
+    (let [response (<! (http/request (merge config {:url endpoint :with-credentials? false})))]
       (cond
-        on-resolve (on-resolve m)
+        (and (:success response) on-resolve)
+        (on-resolve (:body response))
+
         :else
         (println
          "result: "
-         {:response m
+         {:response response
           :config
           {:method (:method config)
            :headers (:headers config)
-           :uri endpoint}}))))))
+           :uri endpoint}})))))
 
 (defn post-event-data
   "Upload data relating to an event by specifying the `event-id`, `filename` and `data`."
   [event-id filename data & [on-resolve]]
-  (try 
+  (try
     (assert event-id "No event-id provided.")
     (assert filename "No filename provided.")
     (assert data "No data provided.")
     (fetch
      (build-uri "events" event-id "upload")
-     {:method "POST"
-      :headers {:x-filename filename}
+     {:method :post
+      :headers {"x-filename" filename}
       :body data}
      on-resolve)
 
